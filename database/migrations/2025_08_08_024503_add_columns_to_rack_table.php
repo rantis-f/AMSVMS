7c900a8364c3240f5c65e0428bce59dc3fe0e828
rantis-f <fayyadrantisismktibazma@gmail.com>
2025-08-08 11:03:31 +0700

    revisi tabel rak

--- database/migrations/2025_08_08_024503_add_columns_to_rack_table.php
+++ database/migrations/2025_08_08_024503_add_columns_to_rack_table.php
@@ -6,30 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('rack', function (Blueprint $table) {
             $table->unsignedInteger('id_fasilitas')->nullable()->after('u');
             $table->unsignedInteger('id_perangkat')->nullable()->after('id_fasilitas');
-
             $table->timestamps();
-            $table->foreign('id_fasilitas')->references('id_fasilitas')->on('listfasilitas')->onDelete('cascade');
-            $table->foreign('id_perangkat')->references('id_perangkat')->on('listperangkat')->onDelete('cascade');
+            $table->foreign('id_fasilitas')->references('id_fasilitas')->on('listfasilitas')->onDelete('set null');
+            $table->foreign('id_perangkat')->references('id_perangkat')->on('listperangkat')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('rack', function (Blueprint $table) {
             $table->dropForeign(['id_fasilitas']);
-            $table->dropForeign(['id_perangkat']);     
-    
+            $table->dropForeign(['id_perangkat']);
             $table->dropColumn(['id_fasilitas', 'id_perangkat', 'created_at', 'updated_at']);
         });
     }
